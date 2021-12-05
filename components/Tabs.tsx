8c6b946566136af5ae377198b03a06c73e246bdc
b9003599 <b9003599@newcastle.ac.uk>
2021-12-05 19:51:12 +0000

    Got bluetooth to work. Still figuring it out

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, PermissionsAndroid} from 'react-native';
+import {BleManager} from 'react-native-ble-plx';
 
 interface props {
     children: React.ReactNode;
@@ -10,12 +11,57 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    text: {
+        fontSize: 100,
+        fontWeight: '900',
+    },
+    button: {
+        margin: 3,
+        padding: 3,
+    },
 });
 
-export default function Tabs({children}: props) {
+export default function Tabs(children: props) {
+    const props = children.route.params.children;
+
+    function onClick() {
+        PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        ]).then(result => {
+            console.log(result);
+        });
+    }
+
+    function scanAndConnect(manager) {
+        manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            // Check if it is a device you are looking for based on advertisement data
+            // or other criteria.
+            if (device.name != null) {
+                console.log(device.name);
+            }
+        });
+    }
+
+    function onClick2() {
+        let manager = new BleManager();
+        scanAndConnect(manager);
+    }
+    // console.log('Log', subscription);
+
     return (
         <View style={styles.view}>
-            <Text>{children}</Text>
+            <Text style={styles.text}>{props}</Text>
+            <View style={styles.button}>
+                <Button title="Blueetooth On" onPress={onClick}></Button>
+            </View>
+            <View style={styles.button}>
+                <Button title="Blueetooth Off" onPress={onClick2}></Button>
+            </View>
         </View>
     );
 }
